a96fdf35fd6971bff2b08f545a2159f1d99f3378
unknown <sunnysebastian234@gmail.com>
2023-10-12 10:16:25 +0530

    second commit

---
+++ Datatypes.java
@@ -0,0 +1,43 @@
+package pkg;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		
+		//Primitive Datatypes
+		
+		byte b=100;
+		System.out.println("Value of b="+b);
+		
+		short s=1875;
+		System.out.println("Value of s="+s);
+		
+		int i=42;
+		System.out.println("Value of i="+i);
+		
+		long l=3265136125783612766l;
+		System.out.println("Value of l="+l);
+		
+		float f1=42.5464f;
+		System.out.println("Value of f1="+f1);
+		
+		double d=242526.62537813547513974597143;
+		System.out.println("Value of d="+d);
+		
+		boolean b1=23>100;
+		System.out.println("Value of b1="+b1);
+		
+		char c1='S';
+		System.out.println("Value of c1="+c1);
+		
+		
+		
+		//Nonprimitive Datatypes
+		
+		String s1="Hello";
+		System.out.println("Value of s1="+s1);
+		
+
+	}
+
+}
